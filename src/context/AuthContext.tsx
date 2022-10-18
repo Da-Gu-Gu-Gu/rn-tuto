5e17326bec447a23b014d72f295e6a0099c60681
hha <heinhtetaung@m-techsolutions.io>
2022-10-18 13:36:31 +0630

    change setState

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,16 +1,19 @@
-import {useState, useContext, createContext} from 'react';
+import {useState, createContext, Dispatch, SetStateAction} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+type Token = Dispatch<SetStateAction<String>>;
 interface ITodo {
-  signin: (x:string,y:string) => void;
-  signout: (s:string,y:string) => void;
-  auth:Boolean
+  signin: (x: string, y: string) => void;
+  signout: (s: string, y: string) => void;
+  token: String;
+  setToken: Token;
 }
 
 const AuthContext = createContext<ITodo>({
-  signin:(x,y)=>{},
-  signout:(x,y)=>{},
-  auth:false
+  signin: (x, y) => {},
+  signout: (x, y) => {},
+  token: '',
+  setToken: () => {},
 });
 
 interface Props {
@@ -18,35 +21,34 @@ interface Props {
 }
 
 const AuthContextProvider = ({children}: Props) => {
+  const [token, setToken] = useState<String>('');
 
-  const [auth,setAuth]=useState<Boolean>(false)
-
-  const value={
-    signin:async (username: string, password: string) => {
+  const value = {
+    signin: async (username: string, password: string) => {
       try {
         await AsyncStorage.setItem('username', username);
         await AsyncStorage.setItem('token', 'token');
-        setAuth((x)=>!x)
-       
+        setToken('aesrer');
       } catch (e) {
         // saving error
       }
     },
     signout: async (username: string, password: string) => {
       try {
-       
         await AsyncStorage.removeItem('username');
         await AsyncStorage.removeItem('token');
-        setAuth((x)=>!x)
+        setToken('');
       } catch (e) {
         // saving error
       }
     },
+  };
 
-
-  }
-
-  return <AuthContext.Provider value={{...value,auth}}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{...value, token, setToken}}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 export default AuthContext;
 export {AuthContextProvider};
--- src/navigation/Navs.tsx
+++ src/navigation/Navs.tsx
@@ -1,42 +1,32 @@
-import React, { useContext } from 'react'
-import AuthContext from '../context/AuthContext'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useContext} from 'react';
+import AuthContext from '../context/AuthContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {NavigationContainer} from '@react-navigation/native';
 import HomeStack from './HomeStack';
 import AuthStack from './AuthStack';
 
 const Navs = () => {
-    const [authorise,setAuthorise]=React.useState<Boolean>(false)
-    const auth=useContext(AuthContext)
+  const [authorise, setAuthorise] = React.useState<Boolean>(false);
+  const auth = useContext(AuthContext);
 
-    React.useEffect(()=>{
-  
-     (async () => { 
-    try {
-    
-      
-      const value = await AsyncStorage.getItem('token')
-    
-      if(value !== null) {
-         
-          setAuthorise(true)
-    
-      }else{
-        setAuthorise(false)
-      }
-    } catch(e) {
- 
-    }
-  })()
+  React.useEffect(() => {
+    (async () => {
+      try {
+        const value = await AsyncStorage.getItem('token');
 
-  
-    },[auth.auth])
+        if (value !== null) {
+          setAuthorise(true);
+        } else {
+          setAuthorise(false);
+        }
+      } catch (e) {}
+    })();
+  }, [auth.auth]);
   return (
     <NavigationContainer>
-        {authorise?<HomeStack />:<AuthStack />}
+      {authorise ? <HomeStack /> : <AuthStack />}
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Navs
-
+export default Navs;
